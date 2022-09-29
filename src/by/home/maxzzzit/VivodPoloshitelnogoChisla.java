111578fce875e3cd45c25dc7cf4d2c6f862c17ff
Maksim Kushner <113466296+MaxZzzIt@users.noreply.github.com>
2022-09-29 23:29:03 +0300

    Вывод на экран (в одну строку) всех целых положительных двухзначных чисел начиная с наименьшего

---
+++ src/by/home/maxzzzit/VivodPoloshitelnogoChisla.java
@@ -0,0 +1,9 @@
+package by.home.maxzzzit;
+
+public class VivodPoloshitelnogoChisla {
+	public static void main(String[] args) {
+		//УСЛОВИЕ: Вывод на экран (в одну строку) всех целых положительных двухзначных чисел начиная с наименьшего
+		for (int i=10; i<100; i++)
+			System.out.print(i+" ");
+	}
+}
